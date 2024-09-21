7786252fce6d3bf454ec0cd170726dc6988b25b2
ViddeshPowar <viddeshpowar@gmail.com>
2024-09-21 14:58:00 +0530

    Third Commit

---
+++ src/JavaPractice/Task_13Sept.java
@@ -0,0 +1,16 @@
+package JavaPractice;
+
+public class Task_13Sept {
+    public static void main(String[] args){
+        double a =34;
+        double b=10;
+        System.out.println("Addition of two number :"+ (a+b));
+        System.out.println("Subtraction of two numbers:"+ (a-b));
+        System.out.println("Multiplication of two numbers:"+ (a*b));
+        System.out.println("Division of two numbers:"+ (a/b));
+        System.out.println("Modulus of two numbers:"+ (a%b));
+
+
+
+    }
+}
