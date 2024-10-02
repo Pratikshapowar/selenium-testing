3d7dd93bcd466439c7e4dcf9a9d6306fbb3d4e53
ViddeshPowar <viddeshpowar@gmail.com>
2024-10-02 21:48:53 +0530

    Table of 10

---
+++ src/Task_25thSept/TableusingLoop.java
@@ -0,0 +1,15 @@
+package Task_25thSept;
+
+public class TableusingLoop {
+    public static void main(String[] args) {
+        int num = 10;
+      /*  for(int i=1;i<=num;i++){
+            System.out.println(num*i);
+        }*/
+        int i = 1;
+        while (i <= 10) {
+            System.out.println(i * num);
+            i++;
+        }
+    }
+}
