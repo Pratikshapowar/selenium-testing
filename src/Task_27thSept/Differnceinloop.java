e7bd2913b5bbc714f96c5dcad3fcadd607f540dd
ViddeshPowar <viddeshpowar@gmail.com>
2024-10-02 22:54:56 +0530

    differencein loop

---
+++ src/Task_27thSept/Differnceinloop.java
@@ -0,0 +1,17 @@
+package Task_27thSept;
+
+public class Differnceinloop {
+    public static void main(String[] args){
+        int i =11;
+        //while first check the condition and then executes the code inide body
+       /* while(i<=10){
+            System.out.println(i);
+            i++;
+        }*/
+       //do while loop executes the code once and then check the condition
+       do{
+           System.out.println(i);
+           i++;
+       }while(i<=10);
+    }
+}
---
+++ src/Task_27thSept/Evenno.java
@@ -0,0 +1,11 @@
+package Task_27thSept;
+
+public class Evenno {
+    public static void main(String[] args){
+        for(int i=1;i<=10;i++){
+            if(i%2==0){
+                System.out.println(i);
+            }
+        }
+    }
+}
