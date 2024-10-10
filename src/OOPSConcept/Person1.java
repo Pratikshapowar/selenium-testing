06dec40a5e4f828996c99b68c99aec6cb11ffeee
ViddeshPowar <viddeshpowar@gmail.com>
2024-10-10 19:48:04 +0530

    Constructor for Initialization

--- src/OOPSConcept/Person1.java
+++ src/OOPSConcept/Person1.java
@@ -26,12 +26,31 @@ public class Person1 {
         this.planet = Planet;
         this.weight = Weight;
     }
-
+    void walking(){
+        System.out.println("Person1 is walking");
+    }
+    int running(){
+        System.out.println("Person1 is running");
+        return(4);
+    }
+    void eating(String dish){
+        System.out.println("Person1 is eating : "+dish);
+    }
+    int driving(String vehicle){
+        System.out.println("Person1 is Driving : "+vehicle);
+        return(2);
+    }
     public static void main(String[] args) {
         Person1 P2 = new Person1();
         Person1 P3 = new Person1("Gray","Earth",55.3f);
         System.out.println("Color of Person1 is : " + P3.color);
+        Person1 P4 = new Person1();
+        P4.walking();
+        P4.running();
 
+        Person1 P5 = new Person1();
+        P5.eating("pizza");
+        P5.driving("Splendor");
 
     }
 }
